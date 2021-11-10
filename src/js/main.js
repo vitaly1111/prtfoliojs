25d82bf69d47e30a49b28722c8f5686ff150bd13
me me <timb0702@gmail.com>
2021-11-10 20:36:33 +0700

    mobile menu added

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 
 const presentOrderBtn=document.querySelector('.present__order-btn')
 const pageOverlayModal=document.querySelector('.page__overlay_modal');
-const modalClose=document.querySelector('.modal__close')
+const modalClose=document.querySelector('.modal__close');
+const headerBurger=document.querySelector('.header__contacts-burger')
+const headerContacts=document.querySelector('.header__contacts')
 
 /* const closeModal=()=>{
 	pageOverlayModal.classList.remove('page__overlay_modal_open')
@@ -13,6 +15,23 @@ presentOrderBtn.addEventListener('click',()=>{
 
 modalClose.addEventListener('click',()=>{closeModal()}) */
 
+const disableScroll=()=>{
+	const widthScroll=window.innerWidth-document.body.offsetWidth;
+	document.body.scrollPosition=window.scrollY
+	document.body.style=`
+	overflow:hidden;
+	position:fixed;
+	top:-${document.body.scrollPosition}+'px';
+	left:0;
+	height:100vh;
+	width:100vw;
+	padding-right:${widthScroll}px;`
+}
+const enableScroll=()=>{
+ document.body.style='position: relative';
+ window.scroll({top: document.body.scrollPosition})
+}
+
 const handlerModal=(openBtn,modal,openSelector,closeTrigger,sk)=>{
 	let opacity=0;
 	const speed={
@@ -21,30 +40,57 @@ const handlerModal=(openBtn,modal,openSelector,closeTrigger,sk)=>{
 		fast:1,
 		default:5
 	};
-	openBtn.addEventListener('click',()=>{
+
+	const openModal=() => {
+		disableScroll()
 		modal.style.opacity=opacity;
 		modal.classList.add(openSelector);
-		const timer=setInterval(()=>{
+		const timer=setInterval(() => {
 			opacity=opacity+0.02
 			modal.style.opacity=opacity;
 			if (opacity>=1) clearInterval(timer)
-		},speed[sk]? speed[sk] : speed.default)
-	})
-	closeTrigger.addEventListener('click',()=>{
-		
+		},speed[sk]? speed[sk]:speed.default)
+	}
+
+	const closeModal=()=>{
 		const timer=setInterval(() => {
 			opacity=opacity-0.02
 			modal.style.opacity=opacity;
 			if (opacity<=0) {
 				clearTimeout(timer);
 				modal.classList.remove(openSelector);
+				enableScroll()
 			}
-				
+
 		},speed[sk]? speed[sk]:speed.default)
+	}
+
+	openBtn.addEventListener('click',openModal)
+	closeTrigger.addEventListener('click',closeModal)
+	modal.addEventListener('click',e=>{
+		if (e.target.classList.contains('page__overlay')){
+			closeModal()
+		}
+	
 	})
 }
 
 handlerModal(presentOrderBtn,pageOverlayModal,'page__overlay_modal_open',modalClose,'slow')
+
+const heandlerBurger=(openBtn,menu,openSelector)=>{
+	openBtn.addEventListener('click',()=>{
+		if(menu.classList.contains(openSelector)){
+			menu.style.height=''
+			menu.classList.remove(openSelector)
+		}else{
+			menu.style.height=menu.scrollHeight+'px'
+			menu.classList.add(openSelector)
+		}
+		
+	})
+}
+
+heandlerBurger(headerBurger,headerContacts,'header__contacts_open')
 
 console.log(presentOrderBtn)
 
